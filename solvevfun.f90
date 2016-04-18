1e070f4f71f76f9fa53abe66001a8c4415961219
Kory D Kantenga <kory.kantenga@gmail.com>
2016-04-18 00:56:14 -0400

    Ex-Post Short Done

--- solvevfun.f90
+++ solvevfun.f90
@@ -195,29 +195,141 @@ END SUBROUTINE vf_exante_l
 ! Solve Ex-Post Short-Term Value Functions
 !------------------------------------------------------------------------------
 SUBROUTINE vf_expost_s(vfa_b,vfb_b,vfp_b,vfa_i,vfb_i,vfp_i, &
-    wfun_bl,wfun_il,wfun_b,wfun_i,pmap,bmap,bprob,pphigrid)
+    wfun_bl,wfun_il,wfun_b,wfun_i,pmap,bmap,bprob,pphigrid,bgrid)
 
     ! Inputs : Ex-Ante Value Functions
     ! Outputs: Ex-Post Short Term Value Functions
 
-    INTEGER bmap,pmap
-    DOUBLE PRECISION, INTENT(in) :: bprob(bmap),pphigrid(pmap)
+    USE mod_globalvar
+    IMPLICIT NONE
+
+    INTEGER bmap,pmap, iCount, bCount
+    DOUBLE PRECISION, INTENT(in) :: bprob(bmap),pphigrid(pmap),bgrid(bmap)
     DOUBLE PRECISION, INTENT(in) :: wfun_b(pmap,bmap),wfun_i(pmap,bmap)
     DOUBLE PRECISION, INTENT(in) :: wfun_bl(2,bmap),wfun_il(2,bmap)
 
-    DOUBLE PRECISION, INTENT(out) :: vfa_b(pmap,bmap),vfa_i(pmap,bmap)
+    DOUBLE PRECISION, INTENT(out) :: vfa_b(pmap),vfa_i(pmap)
     DOUBLE PRECISION, INTENT(out) :: vfb_b(pmap,bmap),vfb_i(pmap,bmap)
-    DOUBLE PRECISION, INTENT(out) :: vfp_b(pmap,bmap),vfp_i(pmap,bmap)
+    DOUBLE PRECISION, INTENT(out) :: vfp_b(pmap),vfp_i(pmap)
 
-    DOUBLE PRECISION uut
-    DOUBLE PRECISION pphia(pmap),pphib(pmap),pphip(pmap)
+    DOUBLE PRECISION uut,probhelp,pphitd(pmap)
+    DOUBLE PRECISION pphia(pmap),pphib(pmap),pphip(pmap)        !Updated phi'
+    DOUBLE PRECISION wfun_b_apr(pmap,bmap),wfun_i_apr(pmap,bmap)!Interpolation W(phi',x),phi'=PPhi_A(phi)
+    DOUBLE PRECISION wfun_b_bpr(pmap,bmap),wfun_i_bpr(pmap,bmap)!Interpolation W(phi',x),phi'=PPhi_B(phi)
+    DOUBLE PRECISION wfun_b_ppr(pmap,bmap),wfun_i_ppr(pmap,bmap)!Interpolation W(phi',x),phi'=PPhi_P(phi)
 
     call PPhiPRIME(pphia,pphib,pphip,pphigrid,pmap)
 
-    !Abstinence PPhi_A(pphipr,pphi)
-    CALL util(uut,qincome)
-    !vfa_b = uut + (1.0-pphigrid)
+    !Interpolate Continuation Values
+    !$OMP PARALLEL DO
+    do iCount = 1,bmap
+        !W_s^bbeta(phi',x)
+        call LinInterp(pmap,pphigrid,wfun_b(:,iCount),pmap,pphia,wfun_b_apr(:,iCount))
+        call LinInterp(pmap,pphigrid,wfun_b(:,iCount),pmap,pphib,wfun_b_bpr(:,iCount))
+        call LinInterp(pmap,pphigrid,wfun_b(:,iCount),pmap,pphip,wfun_b_ppr(:,iCount))
+        !W_s^iiota(phi',x)
+        call LinInterp(pmap,pphigrid,wfun_i(:,iCount),pmap,pphia,wfun_i_apr(:,iCount))
+        call LinInterp(pmap,pphigrid,wfun_i(:,iCount),pmap,pphib,wfun_i_bpr(:,iCount))
+        call LinInterp(pmap,pphigrid,wfun_i(:,iCount),pmap,pphip,wfun_i_ppr(:,iCount))
+    end do
+    !$OMP END PARALELL DO
 
+    !Abstinence, BBETA, PPhi_A(pphipr,pphi)
+    CALL util(uut,qincome)
+    vfa_b = uut + (1.0-pphigrid)*aalpha*bbeta*AAIDS + bbeta*(1.0-((1.0-pphigrid)*aalpha))*( &
+        (1.0-eepsilon)*MATMUL(wfun_b_apr,bprob) + &
+        eepsilon*(pphigrid*MATMUL(wfun_b_apr,bprob) + &
+        (1.0-pphigrid)*DOT_PRODUCT(bprob,wfun_b(1,:))))
+    CALL util(uut,qincome-mcost)
+    vfa_b(1) = uut + bbeta*aalphapr*AAIDS + bbeta*(1.0-aalphapr)*DOT_PRODUCT(bprob,wfun_b(1,:)) !Replace for HIV+ aware
+
+    !Abstinence, IIOTA, PPhi_A(pphipr,pphi)
+    CALL util(uut,qincome)
+    vfa_i = uut + (1.0-pphigrid)*aalpha*iiota*AAIDS + iiota*(1.0-((1.0-pphigrid)*aalpha))*( &
+        (1.0-eepsilon)*(eeta*MATMUL(wfun_b_apr,bprob) + (1.0-eeta)*MATMUL(wfun_i_apr,bprob)) + &
+        eepsilon*(pphigrid*(eeta*MATMUL(wfun_b_apr,bprob) + (1.0-eeta)*MATMUL(wfun_i_apr,bprob)) + &
+        (1.0-pphigrid)*(eeta*DOT_PRODUCT(bprob,wfun_b(1,:)) + (1.0-eeta)*DOT_PRODUCT(bprob,wfun_i(1,:)))))
+    CALL util(uut,qincome-mcost)
+    vfa_i(1) = uut + iiota*aalphapr*AAIDS + iiota*(1.0-aalphapr)*(eeta*DOT_PRODUCT(bprob,wfun_b(1,:)) + &
+        (1.0-eeta)*DOT_PRODUCT(bprob,wfun_i(1,:))) !Replace for HIV+ aware
+
+    !Protected Sex, BBETA, PPhi_P(pphipr,pphi)
+    CALL util(uut,qincome)
+    CALL PR_p(probhelp,pphigrid)
+    pphitd = pphigrid - pphigrid*(1.0-nnu_p)*(1.0-ggamma_p)
+    vfp_b  = uut + ppref_o + bbeta*(1.0-probhelp)*AAIDS + bbeta*(1.0-mmu)*probhelp*( &
+        (1.0-eepsilon)*MATMUL(wfun_b_ppr,bprob) + eepsilon*( &
+        pphigrid*MATMUL(wfun_b_ppr,bprob) + &
+        (1.0-pphigrid)*DOT_PRODUCT(bprob,wfun_b(1,:)))) + &
+        bbeta*mmu*probhelp*(pphitd*DOT_PRODUCT(bprob,wfun_bl(2,:)) + &
+        (1.0-pphitd)*DOT_PRODUCT(bprob,wfun_bl(1,:)))
+    CALL util(uut,qincome-mcost)
+    vfp_b(1) = uut + ppref_o + bbeta*aalphapr*AAIDS + &
+        bbeta*(1.0-mmu)*(1.0-aalphapr)*DOT_PRODUCT(bprob,wfun_b(1,:)) + &
+        bbeta*mmu*DOT_PRODUCT(bprob,wfun_bl(1,:))
+
+    !Protected Sex, IIOTA, PPhi_P(pphipr,pphi)
+    CALL util(uut,qincome)
+    !CALL PR_p(probhelp,pphigrid)
+    !pphitd = pphigrid - pphigrid*(1.0-nnu_p)*(1.0-ggamma_p)
+    vfp_i  = uut + ppref_y + bbeta*(1.0-probhelp)*AAIDS + bbeta*(1.0-mmu)*probhelp*( &
+        (1.0-eepsilon)*(eeta*MATMUL(wfun_b_ppr,bprob) + (1.0-eeta)*MATMUL(wfun_i_ppr,bprob)) + &
+        eepsilon*( &
+        pphigrid*(eeta*MATMUL(wfun_b_ppr,bprob) + (1.0-eeta)*MATMUL(wfun_i_ppr,bprob)) + &
+        (1.0-pphigrid)*(eeta*DOT_PRODUCT(bprob,wfun_b(1,:)) + &
+        (1.0-eeta)*DOT_PRODUCT(bprob,wfun_i(1,:))))) + &
+        bbeta*mmu*probhelp*(pphitd*(eeta*DOT_PRODUCT(bprob,wfun_bl(2,:)) + &
+        (1.0-eeta)*DOT_PRODUCT(bprob,wfun_il(2,:))) + &
+        (1.0-pphitd)*(eeta*DOT_PRODUCT(bprob,wfun_bl(1,:)) + &
+        (1.0-eeta)*DOT_PRODUCT(bprob,wfun_il(1,:))))
+    CALL util(uut,qincome-mcost)
+    vfp_i(1) = uut + ppref_o + bbeta*aalphapr*AAIDS + &
+        bbeta*(1.0-mmu)*(1.0-aalphapr)*(eeta*DOT_PRODUCT(bprob,wfun_b(1,:)) + &
+        (1.0-eeta)*DOT_PRODUCT(bprob,wfun_i(1,:))) + &
+        bbeta*mmu*(1.0-aalphapr)*(eeta*DOT_PRODUCT(bprob,wfun_bl(1,:)) + &
+        (1.0-eeta)*DOT_PRODUCT(bprob,wfun_il(1,:)))
+
+    !Unprotected Sex
+    CALL PR_b(probhelp,pphigrid)
+    pphitd = pphigrid - pphigrid*(1.0-nnu_b)*(1.0-ggamma_b)
+
+    do bCount = 1,bmap
+
+        !Unprotected Sex, BBETA, PPhi_B(pphipr,pphi)
+        CALL util(uut,qincome)
+        vfb_b(:,bCount) = uut + bgrid(bCount) + bbeta*(1.0-probhelp)*AAIDS + &
+            bbeta*(1.0-mmu)*probhelp*( &
+            (1.0-eepsilon)*MATMUL(wfun_b_ppr,bprob) + eepsilon*( &
+            pphigrid*MATMUL(wfun_b_ppr,bprob) + &
+            (1.0-pphigrid)*DOT_PRODUCT(bprob,wfun_b(1,:)))) + &
+            bbeta*mmu*probhelp*(pphitd*DOT_PRODUCT(bprob,wfun_bl(2,:)) + &
+            (1.0-pphitd)*DOT_PRODUCT(bprob,wfun_bl(1,:)))
+        CALL util(uut,qincome-mcost)
+        vfb_b(1,bCount) = uut + bgrid(bCount) + bbeta*aalphapr*AAIDS + &
+            bbeta*(1.0-mmu)*(1.0-aalphapr)*DOT_PRODUCT(bprob,wfun_b(1,:)) + &
+            bbeta*mmu*DOT_PRODUCT(bprob,wfun_bl(1,:))
+
+        !Unprotected Sex, IIOTA, PPhi_B(pphipr,pphi)
+        CALL util(uut,qincome)
+        vfb_i(:,bCount)  = uut + bgrid(bCount) + bbeta*(1.0-probhelp)*AAIDS + &
+            bbeta*(1.0-mmu)*probhelp*( &
+            (1.0-eepsilon)*(eeta*MATMUL(wfun_b_ppr,bprob) + (1.0-eeta)*MATMUL(wfun_i_ppr,bprob)) + &
+            eepsilon*( &
+            pphigrid*(eeta*MATMUL(wfun_b_ppr,bprob) + (1.0-eeta)*MATMUL(wfun_i_ppr,bprob)) + &
+            (1.0-pphigrid)*(eeta*DOT_PRODUCT(bprob,wfun_b(1,:)) + &
+            (1.0-eeta)*DOT_PRODUCT(bprob,wfun_i(1,:))))) + &
+            bbeta*mmu*probhelp*(pphitd*(eeta*DOT_PRODUCT(bprob,wfun_bl(2,:)) + &
+            (1.0-eeta)*DOT_PRODUCT(bprob,wfun_il(2,:))) + &
+            (1.0-pphitd)*(eeta*DOT_PRODUCT(bprob,wfun_bl(1,:)) + &
+            (1.0-eeta)*DOT_PRODUCT(bprob,wfun_il(1,:))))
+        CALL util(uut,qincome-mcost)
+        vfb_i(1,bCount) = uut + bgrid(bCount) + bbeta*aalphapr*AAIDS + &
+            bbeta*(1.0-mmu)*(1.0-aalphapr)*(eeta*DOT_PRODUCT(bprob,wfun_b(1,:)) + &
+            (1.0-eeta)*DOT_PRODUCT(bprob,wfun_i(1,:))) + &
+            bbeta*mmu*(1.0-aalphapr)*(eeta*DOT_PRODUCT(bprob,wfun_bl(1,:)) + &
+            (1.0-eeta)*DOT_PRODUCT(bprob,wfun_il(1,:)))
+
+    end do
 
 END SUBROUTINE vf_expost_s
 
--- subroutines.f90
+++ subroutines.f90
@@ -111,18 +111,18 @@ SUBROUTINE PR_b(prob,pphi)
 
     DOUBLE PRECISION pphi,prob
 
-    prob = 1.0 - aalpha*((1.0-pphi)+(1.0-nnu_b)*(1.0-ggamma_b))
+    prob = 1.0 - aalpha*((1.0-pphi)+pphi*(1.0-nnu_b)*(1.0-ggamma_b))
 
 END SUBROUTINE PR_b
 !------------------------------------------------------------------------------
 SUBROUTINE PR_p(prob,pphi)
-
+!Prob(No Aids Symptoms|Protected Sex)
     USE mod_globalvar
     IMPLICIT NONE
 
     DOUBLE PRECISION pphi,prob
 
-    prob = 1.0 - aalpha*((1.0-pphi)+(1.0-nnu_p)*(1.0-ggamma_p))
+    prob = 1.0 - aalpha*((1.0-pphi) + pphi*(1.0-nnu_p)*(1.0-ggamma_p))
 
 END SUBROUTINE PR_p
 !------------------------------------------------------------------------------
@@ -137,4 +137,90 @@ SUBROUTINE PR_a(prob,pphi)
 
 END SUBROUTINE PR_a
 !------------------------------------------------------------------------------
+
+!------------------------------------------------------------------------------
+! Linear Interpolation of Value Function
+!------------------------------------------------------------------------------
+SUBROUTINE LinInterp (n,x,y,ni,xi,yi)
+    !this does linear interpolation of (x,y) at points xi
+    !requires x to be sorted in ascending order
+    !extrapolates out of range
+    IMPLICIT NONE
+    INTEGER, INTENT(in)        :: n,ni
+    real(8), INTENT(in)    :: x(:),y(:),xi(:)
+    real(8), INTENT(out)    :: yi(:)
+    real(8), DIMENSION(ni)    :: xL,xH,yL,yH
+    INTEGER                    :: i,locL(ni)
+
+
+    DO i = 1,ni
+
+        LocL(i) = MAXLOC(x,1,MASK=xi(i)>x)
+
+        IF (xi(i)<=x(1)) THEN
+            LocL(i) = 1
+        END IF
+
+        IF (LocL(i)>=n) THEN
+            LocL(i) = n-1
+        END IF
+
+
+        xL(i) = x(locL(i))
+        xH(i) = x(locL(i)+1)
+        yL(i) = y(locL(i))
+        yH(i) = y(locL(i)+1)
+
+        yi(i) = yL(i) + (xi(i)-xL(i))*((yH(i)-yL(i))/(xH(i)-xL(i)))
+
+    END DO
+
+END SUBROUTINE LinInterp
+
+
+    SUBROUTINE BiLinInterp1(nx,x,ny,y,f,xi,yi,fi)
+        !this does linear interpolation of f(x,y) at points (xi,yi)
+        !requires x to be sorted in ascending order
+        !
+        !extrapolates out of range
+        IMPLICIT NONE
+        INTEGER, INTENT(in)        :: nx,ny
+        real(8), INTENT(in)    :: x(:),y(:),f(:,:),xi,yi
+        real(8), INTENT(out)    :: fi
+        real(8)                :: xL,xH,yL,yH,fLL,fHH,fLH,fHL,dxdy
+        INTEGER                    :: xlocL,ylocL
+
+        xlocL = MAXLOC(x,1,MASK=xi>x)
+        ylocL = MAXLOC(y,1,MASK=yi>y)
+
+        IF (xi<=x(1)) THEN
+            xlocL = 1
+        END IF
+
+        IF (xLocL>=nx) THEN
+            xLocL = nx-1
+        END IF
+
+        IF (yi<=y(1)) THEN
+            ylocL = 1
+        END IF
+
+        IF (yLocL>=ny) THEN
+            yLocL = ny-1
+        END IF
+
+        xL  = x(xlocL)
+        xH  = x(xlocL +1)
+        yL  = y(ylocL)
+        yH  = y(ylocL +1)
+        fLL = f(xlocL,ylocL)
+        fLH = f(xlocL,ylocL+1)
+        fHL = f(xlocL+1,ylocL)
+        fHH = f(xlocL+1,ylocL+1)
+
+        dxdy = (xH-xL)*(yH-yL)
+        fi = fLL*(xH-xi)*(yH-yi)/(dxdy) + fHL*(xi-xL)*(yH-yi)/(dxdy) + fLH*(xH-xi)*(yi-yL)/(dxdy) + fHH*(xi-xL)*(yi-yL)/(dxdy)
+
+
+    END SUBROUTINE BiLinInterp1
 
